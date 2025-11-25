1e8e6b22875877dbe0da989f7a7e7b9b5afcab9d
Zehkira <9485872-zehkira@users.noreply.gitlab.com>
2025-11-25 12:38:38 +0100

    Release v4.2.0 zehkira/monophony!128

--- source/monophony/__init__.py
+++ source/monophony/__init__.py
@@ -4,7 +4,7 @@ import pathlib
 import logboth
 
 
-__version__ = '4.1.2'
+__version__ = '4.2.0'
 ID = 'io.gitlab.zehkira.Monophony' # Full ID per Freedesktop standards
 NAME = 'monophony' # Use for app executable, directories and so on
 DISPLAY_NAME = 'Monophony' # For window titles and such - do not use in logic
